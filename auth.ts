716cc455e0b79a50c755d048f1185a01a46b61d5
emmanbobo <olawalealawode@gmail.com>
2025-05-05 16:02:48 +0100

    auth.ts

--- auth.ts
+++ auth.ts
@@ -7,7 +7,7 @@ import { prisma } from "@/lib/prisma"
 import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
-import { NextAuthOptions } from "next-auth"
+
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
